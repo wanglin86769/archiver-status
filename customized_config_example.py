af4c674ce4d12d4736ad6e8a72d254162ae57063
Yong Hu <yhu@bnl.gov>
2024-10-14 14:42:39 -0400

    Configuration should be done by user-provided 'customized_config.py' instead of modifying the source code 'archiver_status.py'

---
+++ customized_config_example.py
@@ -0,0 +1,11 @@
+# Customized Configurations
+appliances = [
+    # For single node deployment of Archiver Appliance, configure url and identity for the node
+    { 'url': 'http://localhost:10065', 'identity': 'appliance0' },   
+    # For cluster deployment of Archiver Appliance, configure url and identity for all the nodes
+    # { 'url': 'http://10.1.236.142:17665', 'identity': 'appliance_01' },
+    # { 'url': 'http://10.1.236.143:17665', 'identity': 'appliance_02' },
+]
+REQUEST_TIMEOUT = 10
+REQUEST_INTERVAL = 10
+prefix = 'arcapp-acc:'
