67d264712f21a5c4a637c0050717c601bf0ba174
Yong Hu <yhu@bnl.gov>
2024-10-14 15:08:25 -0400

    use 17665 as the default port in customized_config_example.py

--- customized_config_example.py
+++ customized_config_example.py
@@ -1,7 +1,7 @@
 # Customized Configurations
 appliances = [
     # For single node deployment of Archiver Appliance, configure url and identity for the node
-    { 'url': 'http://localhost:10065', 'identity': 'appliance0' },   
+    { 'url': 'http://localhost:17665', 'identity': 'appliance0' },
     # For cluster deployment of Archiver Appliance, configure url and identity for all the nodes
     # { 'url': 'http://10.1.236.142:17665', 'identity': 'appliance_01' },
     # { 'url': 'http://10.1.236.143:17665', 'identity': 'appliance_02' },
